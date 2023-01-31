78ae6a4c1e0753b924c71c420142b859c3dc1bf3
Navruz <navuz.ochilov98@mail.ru>
2023-01-31 16:03:25 +0300

    done

--- server/routers/reports/reports.js
+++ server/routers/reports/reports.js
@@ -490,53 +490,83 @@ module.exports.getDebtsReport = async (req, res) => {
         .json({ message: `Diqqat! Do'kon haqida malumotlar topilmadi!` });
     }
 
-    const connectors = await DailySaleConnector.find({
+    const saleconnector = await SaleConnector.find({
       market,
       createdAt: {
         $gte: startDate,
         $lte: endDate,
       },
     })
-      .sort({ createdAt: -1 })
       .select('-isArchive -updatedAt -__v -packman')
-      .populate('products', '-__v -isArchive -updatedAt')
-      .populate('debt', '-__v -isArchive -updatedAt')
-      .populate('discount', '-__v -isArchive -updatedAt')
-      .populate('payment', '-__v -isArchive -updatedAt')
+      .populate(
+        'payments',
+        'cash cashuzs card carduzs transfer transferuzs payment paymentuzs totalprice totalpriceuzs'
+      )
       .populate({
-        path: 'saleconnector',
-        select: "id client",
+        path: 'products',
+        select:
+          'totalprice unitprice totalpriceuzs unitpriceuzs pieces createdAt discount saleproducts product',
+        options: { sort: { createdAt: -1 } },
         populate: {
-          path: 'client',
-          select: "-__v -isArchive -updatedAt"
-        }
+          path: 'product',
+          select: 'productdata',
+          populate: { path: 'productdata', select: 'name code' },
+        },
       })
-      .lean()
+      .populate({
+        path: 'products',
+        select:
+          'totalprice unitprice totalpriceuzs unitpriceuzs pieces createdAt discount saleproducts product',
+        options: { sort: { createdAt: -1 } },
+        populate: {
+          path: 'user',
+          select: 'firstname lastname',
+        },
+      })
+      .populate('client', 'name')
+      .populate(
+        'payments',
+        'payment paymentuzs comment totalprice totalpriceuzs'
+      )
+      .populate('debts', 'comment')
+      .populate(
+        'discounts',
+        'discount discountuzs procient products totalprice totalpriceuzs'
+      )
+      .populate('user', 'firstname lastname')
+      .populate('dailyconnectors', 'comment debt')
+      .lean();
 
-    const debtsreport = connectors
+    const debtsreport = saleconnector
       .map((sale) => {
         const reduce = (arr, el) =>
           arr.reduce((prev, item) => prev + (item[el] || 0), 0);
-        const discount = sale.discount && sale.discount.discount || 0
-        const discountuzs = sale.discount && sale.discount.discountuzs || 0
-        console.log(sale._id, sale.payment);
-        const payment = sale.payment.payment;
-        const paymentuzs = sale.payment.paymentuzs;
+        const discount = reduce(sale.discounts, 'discount');
+        const discountuzs = reduce(sale.discounts, 'discountuzs');
+        const payment = reduce(sale.payments, 'payment');
+        const paymentuzs = reduce(sale.payments, 'paymentuzs');
         const totalprice = reduce(sale.products, 'totalprice');
         const totalpriceuzs = reduce(sale.products, 'totalpriceuzs');
 
+        const debtComment =
+          sale.debts.length > 0
+            ? sale.debts[sale.debts.length - 1].comment
+            : '';
+        const debtId =
+          sale.debts.length > 0 ? sale.debts[sale.debts.length - 1]._id : '';
+
         return {
           _id: sale._id,
-          id: sale.saleconnector.id,
+          id: sale.id,
           createdAt: sale.createdAt,
-          client: sale.saleconnector.client && sale.saleconnector.client,
+          client: sale.client && sale.client,
           totalprice,
           totalpriceuzs,
           debt: Math.round((totalprice - payment - discount) * 1000) / 1000,
           debtuzs:
             Math.round((totalpriceuzs - paymentuzs - discountuzs) * 1) / 1,
-          debtid: sale.debt && sale.debt._id,
-          comment: sale.debt && sale.debt.comment,
+          debtid: debtId,
+          comment: debtComment,
           saleconnector: { ...sale },
         };
       })
@@ -544,7 +574,6 @@ module.exports.getDebtsReport = async (req, res) => {
 
     res.status(201).json({ data: debtsreport });
   } catch (error) {
-    console.log(error);
     res.status(400).json({ error: 'Serverda xatolik yuz berdi...' });
   }
 };
