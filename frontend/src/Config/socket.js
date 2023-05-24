962d1353371af2306250f92b151cad0cdc0474e0
Navruz Ochilov <navruz.ochilov98@mail.ru>
2023-05-24 13:51:23 +0500

    Update socket.js

--- frontend/src/Config/socket.js
+++ frontend/src/Config/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-const baseURL = process.env.REACT_APP_API_SOCKET_ENDPOINT || 'http://alo24.uz'
+const baseURL = process.env.REACT_APP_API_SOCKET_ENDPOINT || 'http://45.87.3.238'
 
 const socket = io(baseURL)
 const userData = JSON.parse(localStorage.getItem('userData'))
